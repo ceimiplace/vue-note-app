fa1b82bb805b6d63fa448f14026f0bcaabe0feaa
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2023-07-23 22:34:47 +0300

    added vue logic

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,66 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const showModal = ref(false);
+const note = ref("");
+const notes = ref([]);
+const showModalError = ref(false);
+function getRandomColor() {
+  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+}
+function addNote() {
+  if (note.value.length < 10) {
+    showModalError.value = true;
+    return;
+  }
+  notes.value.push({
+    text: note.value,
+    date: new Date(),
+    id: Math.floor(Math.random() * 1000),
+    color: getRandomColor(),
+  });
+  note.value = "";
+  showModal.value = false;
+}
+</script>
 <template>
   <main>
-    <div class="module-container">
+    <div v-if="showModal" class="module-container">
       <div class="module">
         <textarea
+          v-model.trim="note"
           class="module-text"
           name=""
           id=""
           cols="30"
           rows="10"
         ></textarea>
-        <button class="module-button add-button">Add Note</button>
-        <button class="module-button close-button">Close</button>
+        <p class="module-error" v-if="showModalError">
+          Please enter at least 10 valid characters
+        </p>
+        <button class="module-button add-button" @click="addNote">
+          Add Note
+        </button>
+        <button class="module-button close-button" @click="showModal = false">
+          Close
+        </button>
       </div>
     </div>
     <div class="container">
       <header>
         <h1 class="header-heading">Notes</h1>
-        <button class="header-button">+</button>
+        <button class="header-button" @click="showModal = true">+</button>
       </header>
       <section class="cards-container">
-        <div class="card">
-          <p class="card-text">text-text</p>
-          <p class="card-date">date-date-</p>
-        </div>
-        <div class="card">
-          <p class="card-text">text-text</p>
-          <p class="card-date">date-date-</p>
-        </div>
-        <div class="card">
-          <p class="card-text">text-text</p>
-          <p class="card-date">date-date-</p>
-        </div>
-        <div class="card">
-          <p class="card-text">text-text</p>
-          <p class="card-date">date-date-</p>
-        </div>
-        <div class="card">
-          <p class="card-text">text-text</p>
-          <p class="card-date">date-date-</p>
-        </div>
-        <div class="card">
-          <p class="card-text">text-text</p>
-          <p class="card-date">date-date-</p>
+        <div
+          class="card"
+          v-for="userNote in notes"
+          :style="{ backgroundColor: userNote.color }"
+        >
+          <p class="card-text">{{ userNote.text }}</p>
+          <p class="card-date">
+            {{ userNote.date.getDate() }}/{{ userNote.date.getMonth() + 1 }} /
+            {{ userNote.date.getFullYear() }}
+          </p>
         </div>
       </section>
     </div>
@@ -116,6 +134,8 @@ main {
   width: 200px;
   min-height: 200px;
   background-color: aliceblue;
+  border-radius: 10px;
+  padding: 10px;
 }
 .header-heading {
   font-size: 40px;
@@ -130,5 +150,8 @@ main {
   height: 40px;
   border: none;
   cursor: pointer;
+}
+.module-error {
+  color: red;
 }
 </style>
