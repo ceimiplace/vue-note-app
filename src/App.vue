9656b3b20cc8ed992f45b110c4177848c53f2b8c
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2023-07-23 14:38:38 +0300

    Added html markup
    added skeleton html

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,31 @@
 <script setup></script>
-<template></template>
+<template>
+  <main>
+    <div class="module-container">
+      <div class="module">
+        <textarea
+          class="module-text"
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button class="module-button addition-button">+</button>
+        <button class="module-button subtraction-button">-</button>
+      </div>
+    </div>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+      <section class="cards-container">
+        <div class="card">
+          <p class="card-text"></p>
+          <p class="card-date"></p>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
 <style scoped></style>
